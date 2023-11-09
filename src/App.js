ea46cd6d4be8eeb1bf8fa5c6bb43a2eeda8b7f5d
dasom <zzzoo42@naver.com>
2023-11-09 15:37:45 +0900

    게시불 보기 페이지 작성

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ import {
 import { HomeLayout } from "./layout/HomeLayout";
 import { BoardList } from "./page/BoardList";
 import { BoardWrite } from "./page/BoardWrite";
+import { BoardView } from "./page/BoardView";
 
 const routes = createBrowserRouter(
   // 전체 화면의 틀
@@ -16,6 +17,7 @@ const routes = createBrowserRouter(
       {/*페이지가 변경될때 보이는 컴포넌트 */}
       <Route index element={<BoardList />} />
       <Route path="write" element={<BoardWrite />} />
+      <Route path="board/:id" element={<BoardView />} />
     </Route>,
   ),
 );
