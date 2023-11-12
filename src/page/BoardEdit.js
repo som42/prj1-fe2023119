c4d40874b1562d07d6fe42b048413eedfbde15b1
som42 <zzzoo42@naver.com>
2023-11-12 19:19:12 +0900

    제목과 콘텐트 변경 가능 handle ... 함수 완성

--- src/page/BoardEdit.js
+++ src/page/BoardEdit.js
@@ -27,16 +27,28 @@ export function BoardEdit() {
     return <Spinner />;
   }
 
+  function handleTitleChange(e) {
+    updateBoard((draft) => {
+      draft.title = e.target.value;
+    });
+  }
+
+  function handleContentChange(e) {
+    updateBoard((draft) => {
+      draft.content = e.target.value;
+    });
+  }
+
   return (
     <Box>
       <h1>{id}번 글 수정</h1>
       <FormControl>
         <FormLabel>제목</FormLabel>
-        <Input value={board.title} />
+        <Input value={board.title} onChange={handleTitleChange} />
       </FormControl>
       <FormControl>
         <FormLabel>본문</FormLabel>
-        <Textarea value={board.content} />
+        <Textarea value={board.content} onChange={handleContentChange} />
       </FormControl>
       <FormControl>
         <FormLabel>작성자</FormLabel>
