17c4993577b89ed7964e273ef68a5fa4afb2b7c2
som42 <zzzoo42@naver.com>
2023-11-12 20:06:45 +0900

    저장 버튼 클릭시 put / api/board/edit 요청

--- src/page/BoardEdit.js
+++ src/page/BoardEdit.js
@@ -30,6 +30,16 @@ export function BoardEdit() {
     return <Spinner />;
   }
 
+  function handleSubmit() {
+    //   저장 버튼 클릭 시
+    // PUT /api/board/edit
+    axios
+      .put("/api/board/edit", board)
+      .then(() => console.log("잘됨"))
+      .catch(() => console.log("잘안됨"))
+      .finally(() => console.log("끝"));
+  }
+
   return (
     <Box>
       <h1>{id}번 글 수정</h1>
@@ -66,7 +76,9 @@ export function BoardEdit() {
           }}
         />
       </FormControl>
-      <Button colorScheme="blue">저장</Button>
+      <Button colorScheme="blue" onClick={handleSubmit}>
+        저장
+      </Button>
       {/* navigate(-1) : 이전 경로로 이동*/}
       <Button onClick={() => navigate(-1)}>취소</Button>
     </Box>
