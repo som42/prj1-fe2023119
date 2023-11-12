bc1fc892948f1888c6c69757fa0f829996234c41
som42 <zzzoo42@naver.com>
2023-11-12 22:12:53 +0900

    ID 중복 체크 기능 추가

--- src/page/member/MemberSignup.js
+++ src/page/member/MemberSignup.js
@@ -1,6 +1,7 @@
 import {
   Box,
   Button,
+  Flex,
   FormControl,
   FormErrorMessage,
   FormLabel,
@@ -15,7 +16,13 @@ export function MemberSignup() {
   const [passwordCheck, setPasswordCheck] = useState("");
   const [email, setEmail] = useState("");
 
+  const [idAvailable, setIdAvailable] = useState(false);
+
   let submitAvailable = true;
+
+  if (idAvailable) {
+    submitAvailable = false;
+  }
 
   if (password != passwordCheck) {
     submitAvailable = false;
@@ -36,12 +43,38 @@ export function MemberSignup() {
       .finally(() => console.log("done"));
   }
 
+  function handleIdCheck() {
+    const searchParam = new URLSearchParams();
+    searchParam.set("id", id);
+
+    axios
+      .get("/api/member/check?" + searchParam.toString())
+      .then(() => {
+        setIdAvailable(false);
+      })
+      .catch((error) => {
+        if (error.response.staus === 404) {
+          setIdAvailable(true);
+        }
+      });
+  }
+
   return (
     <Box>
       <h1>회원 가입</h1>
-      <FormControl>
+      <FormControl isInvalid={!idAvailable}>
         <FormLabel>id</FormLabel>
-        <input value={id} onChange={(e) => setId(e.target.value)} />
+        <Flex>
+          <input
+            value={id}
+            onChange={(e) => {
+              setId(e.target.value);
+              setIdAvailable(false);
+            }}
+          />
+          <Button onClick={handleIdCheck}>중복확인</Button>
+        </Flex>
+        <FormErrorMessage>ID 중복체크를 해주세요.</FormErrorMessage>
       </FormControl>
       <FormControl isInvalid={password.length === 0}>
         <FormLabel>password</FormLabel>
