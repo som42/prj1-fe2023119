44b17058524df5e1e3d902b9f2328ddcc2a4f36d
som42 <zzzoo42@naver.com>
2023-11-13 09:39:03 +0900

    회원 정보보기 기능 추가중

--- src/page/member/MemberView.js
+++ src/page/member/MemberView.js
@@ -1,8 +1,43 @@
 import { useSearchParams } from "react-router-dom";
+import { useEffect, useState } from "react";
+import {
+  Box,
+  Button,
+  FormControl,
+  FormLabel,
+  Input,
+  Spinner,
+} from "@chakra-ui/react";
+import axios from "axios";
 
 export function MemberView() {
+  const [member, setMember] = useState(null);
   // /member?id=userid
   const [params] = useSearchParams();
 
-  return <div>{params.get("id")} 님 회원 정보 보기</div>;
+  useEffect(() => {
+    axios
+      .get("/api/member?" + params.toString())
+      .then((response) => setMember(response.data));
+  }, []);
+
+  if (member === null) {
+    return <Spinner />;
+  }
+
+  return (
+    <Box>
+      <h1>{member.id}님 정보</h1>
+      <FormControl>
+        <FormLabel>password</FormLabel>
+        <Input type="text" value={member.password} readOnly />
+      </FormControl>
+      <FormControl>
+        <FormLabel>email</FormLabel>
+        <Input value={member.email} readOnly />
+      </FormControl>
+      <Button colorScheme="purple">수정</Button>
+      <Button colorScheme="red">삭제</Button>
+    </Box>
+  );
 }
