fa42cf1cd1e7a9e649225cf60b837d64f8bee1a7
som42 <zzzoo42@naver.com>
2023-11-14 11:39:26 +0900

    글 수정시 writer Input 삭제

--- src/component/NavBar.js
+++ src/component/NavBar.js
@@ -6,17 +6,29 @@ export function NavBar() {
   const navigate = useNavigate();
 
   function handleLogout() {
-    axios.post("/api/member/logut").then(() => console.log("로그아웃 성공"));
+    axios.post("/api/member/logout").then(() => console.log("로그아웃 성공"));
   }
 
   return (
     <Flex>
-      <Button onClick={() => navigate("/")}>home</Button>
-      <Button onClick={() => navigate("/write")}>write</Button>
-      <Button onClick={() => navigate("/signup")}>signup</Button>
-      <Button onClick={() => navigate("/member/list")}>회원목록</Button>
-      <Button onClick={() => navigate("/login")}>로그인 </Button>
-      <Button onClick={handleLogout}>로그아웃</Button>
+      <Button bg={"red.100"} onClick={() => navigate("/")}>
+        home
+      </Button>
+      <Button bg={"red.200"} onClick={() => navigate("/write")}>
+        write
+      </Button>
+      <Button bg={"red.300"} onClick={() => navigate("/signup")}>
+        signup
+      </Button>
+      <Button bg={"red.400"} onClick={() => navigate("/member/list")}>
+        회원목록
+      </Button>
+      <Button bg={"red.500"} onClick={() => navigate("/login")}>
+        로그인{" "}
+      </Button>
+      <Button bg={"red.600"} onClick={handleLogout}>
+        로그아웃
+      </Button>
     </Flex>
   );
 }
