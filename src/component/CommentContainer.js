2ca01971b2e123038e57fd48c7525292986ea519
som42 <zzzoo42@naver.com>
2023-11-15 13:57:05 +0900

    delete 연습

--- src/component/CommentContainer.js
+++ src/component/CommentContainer.js
@@ -6,13 +6,23 @@ import {
   CardHeader,
   Flex,
   Heading,
+  Modal,
+  ModalBody,
+  ModalCloseButton,
+  ModalContent,
+  ModalFooter,
+  ModalHeader,
+  ModalOverlay,
   Stack,
   StackDivider,
   Text,
   Textarea,
+  useDisclosure,
+  useToast,
 } from "@chakra-ui/react";
-import { useEffect, useState } from "react";
+import React, { useEffect, useState } from "react";
 import axios from "axios";
+import { useNavigate, useSearchParams } from "react-router-dom";
 
 function CommentForm({ boardId, isSubmitting, onSubmit }) {
   const [comment, setComment] = useState("");
@@ -32,6 +42,38 @@ function CommentForm({ boardId, isSubmitting, onSubmit }) {
 }
 
 function CommentList({ commentList }) {
+  const params = useSearchParams();
+
+  const { isOpen, onClose, onOpen } = useDisclosure();
+
+  const navigate = useNavigate();
+  const toast = useToast();
+  function handleDelete() {
+    axios
+      .delete("/api/comment/list" + params.toString())
+      .then(() => {
+        toast({
+          description: "삭제 되었습니다",
+          status: "success",
+        });
+        navigate("/");
+      })
+      .catch((error) => {
+        if (error.response.status === 401 || error.response.status === 403) {
+          toast({
+            description: "권한이 없습니다.",
+            status: "error",
+          });
+        } else {
+          toast({
+            description: "삭제 처리 중에 문제가 발생하였습니다.",
+            status: "error",
+          });
+        }
+      })
+      .finally(() => onclose());
+  }
+
   return (
     <Card>
       <CardHeader>
@@ -48,6 +90,23 @@ function CommentList({ commentList }) {
               <Text sx={{ whiteSpace: "pre-wrap" }} pt="2" fontSize="sm">
                 {comment.comment}
               </Text>
+              <Button>삭제</Button>
+              {/* 삭제 모달 */}
+              <Modal isOpen={isOpen} onClose={onClose}>
+                <ModalOverlay />
+                <ModalContent>
+                  <ModalHeader>삭제 확인</ModalHeader>
+                  <ModalCloseButton />
+                  <ModalBody>삭제 하시겠습니까?</ModalBody>
+
+                  <ModalFooter>
+                    <Button onClick={onClose}>닫기</Button>
+                    <Button onClick={handleDelete} colorScheme="red">
+                      삭제
+                    </Button>
+                  </ModalFooter>
+                </ModalContent>
+              </Modal>
             </Box>
           ))}
         </Stack>
@@ -57,10 +116,6 @@ function CommentList({ commentList }) {
 }
 
 export function CommentContainer({ boardId }) {
-  const [isSubmitting, setIsSubmitting] = useState(false);
-
-  const [commentList, setCommentList] = useState([]);
-
   useEffect(() => {
     if (!isSubmitting) {
       const params = new URLSearchParams();
