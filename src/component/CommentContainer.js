af258e6ae36528bc35e99d1e01bae887bc333444
som42 <zzzoo42@naver.com>
2023-11-15 10:33:12 +0900

    댓글 목록

--- src/component/CommentContainer.js
+++ src/component/CommentContainer.js
@@ -1,6 +1,6 @@
 import { border, Box, Button, Input, Textarea } from "@chakra-ui/react";
 import axios from "axios";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 function CommentForm({ boardId }) {
   const [comment, setComment] = useState("");
@@ -22,7 +22,18 @@ function CommentForm({ boardId }) {
   );
 }
 
-function CommentList() {
+function CommentList({ boardId }) {
+  const [commentList, setCommentList] = useState(null);
+
+  useEffect(() => {
+    const params = new URLSearchParams();
+    params.set("id", boardId);
+
+    axios
+      .get("/api/comment/list?" + params)
+      .then((response) => setCommentList(response.data));
+  }, []);
+
   return <Box>댓글 리스트</Box>;
 }
 
@@ -30,7 +41,7 @@ export function CommentContainer({ boardId }) {
   return (
     <Box>
       <CommentForm boardId={boardId} />
-      <CommentList />
+      <CommentList boardId={boardId} />
     </Box>
   );
 }
